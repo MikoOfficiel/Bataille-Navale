0ec11977d6a7af7f2ebc8ef1553480f2e4bbcad6
DjamTam <116735174+DjamTam@users.noreply.github.com>
2022-10-27 15:24:16 +0200

    Grille_de_jeu V1

--- src/Bataille_Navale/Grille_de_jeu.java
+++ src/Bataille_Navale/Grille_de_jeu.java
@@ -1,7 +1,24 @@
 package Bataille_Navale;
 
 public class Grille_de_jeu {
-	
-	//hello;
 
-}
+		//Attributs 
+		private int Ligne;
+		private int Colonnes;
+		private char [][] grille;
+
+		//Constructeur
+		public Grille_de_jeu(int Ligne, int Col){
+
+		this.Ligne = Ligne;
+		this.Colonnes = Col;
+		grille = new char[this.Ligne][this.Colonnes];
+		
+		for(int i = 0; i<this.Ligne;i++) {
+			for (int j=0; j <this.Colonnes; j++) {
+				
+			}
+		}
+		
+		}
+	}
