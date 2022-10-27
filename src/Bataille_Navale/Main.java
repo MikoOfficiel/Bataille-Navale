ffe0d199f9f723105c534c5be1876307020d36e5
DjamTam <116735174+DjamTam@users.noreply.github.com>
2022-10-27 15:53:12 +0200

    Grille V2

---
+++ src/Bataille_Navale/Main.java
@@ -0,0 +1,12 @@
+package Bataille_Navale;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Grille_de_jeu g1 = new Grille_de_jeu(15,15);
+		g1.Afficher();
+	}
+
+}
